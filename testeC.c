4a974244f833f843da9d2d25628fff92c6699613
Marcus Vinicius <vinizinco@gmail.com>
2026-02-24 12:38:38 -0300

    feat: adiciona programa area de retangulo em C

---
+++ testeC.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() //programa para determinar comprimento e largura de retangulo
+{
+    int largura, comprimento, area;
+
+    printf("Digite a Largura do retangulo: \n");
+    scanf("%d", &largura); 
+
+    printf ("Digite o comprimento do retangulo: \n");
+    scanf("%d", &comprimento);w
+
+    area = largura * comprimento; 
+
+    printf("A area do retangulo e: %d\n" area,);
+    return 0;
+}
